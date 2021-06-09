afb7e1944a8b898ba22e5b7e72699ebd4fa48042
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-09 15:29:33 +0100

    glfw works -_-

--- physical-modelling-lib/src/main.cpp
+++ physical-modelling-lib/src/main.cpp
@@ -1,12 +1,15 @@
 #include <pybind11/pybind11.h>	// python bindings
-// #include <GLFW/glfw3.h>		// GPU helper
+#include <GLFW/glfw3.h>			// OpenGL
+// #include <GL/glew.h>
 
+// python namespace
 namespace py = pybind11;
 
 float add_floats(float x, float y) {
 	return x + y;
 }
 
+// configure python bindings
 PYBIND11_MODULE(physical_lib, handle) {
 	handle.doc() = "Physical modelling library";
 	handle.def("add", &add_floats);
