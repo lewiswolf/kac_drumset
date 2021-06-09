24d0a91763fc2dd5c6e10adf42c27bc1b7ad1436
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-09 15:41:16 +0100

    rearranged install and compile commands

--- bin/compile_cpp.sh
+++ bin/compile_cpp.sh
@@ -1,3 +1,10 @@
+# check if cmake exists
+if ! command -v cmake >/dev/null
+then
+	echo "\033[0;91mWARNING\033[0m: please install cmake to use this code."
+	exit
+fi
+
 # clear build folder (used for testing)
 cd physical-modelling-lib/build
 shopt -s extglob
--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,3 @@
-# check if cmake exists
-if ! command -v cmake >/dev/null
-then
-	echo "\033[0;91mWARNING\033[0m: please install cmake to use this code."
-	exit
-fi
-
 # install cpp dependencies
 cd physical-modelling-lib/includes
 	git clone -b v2.6.2 https://github.com/pybind/pybind11.git
