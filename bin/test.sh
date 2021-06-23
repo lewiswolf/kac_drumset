933f545f66587f61a7e450ba47a04360ce036995
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-23 17:20:55 +0100

    misplaced comment

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,5 @@
+pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py
+
 # arg is used to stop mypy getting annoyed about the src folder...
 # could/should probably be deleted in the future
-pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py
 pipenv run mypy --ignore-missing-imports index.py 
