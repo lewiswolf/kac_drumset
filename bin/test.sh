a11e11d74680b61ac83a3c5b44c26969976ffeea
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-23 17:18:18 +0100

    added flake8 to test

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,4 @@
 # arg is used to stop mypy getting annoyed about the src folder...
 # could/should probably be deleted in the future
-pipenv run mypy index.py --ignore-missing-imports 
+pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py
+pipenv run mypy --ignore-missing-imports index.py 
