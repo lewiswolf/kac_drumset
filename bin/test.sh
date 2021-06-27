862ca90524df5e3d3cc72aeaf132d7b3dfe102e1
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-28 00:42:45 +0100

    comments about --ignore-missing-imports argument

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
 pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py src
 
-# arg is used to stop mypy getting annoyed about the src folder...
-# could/should probably be deleted in the future
+# --ignore-missing-imports is used to stop mypy getting annoyed at dependencies..
+# could/should probably be deleted in the future (may be to do with pipenv)
 pipenv run mypy --ignore-missing-imports index.py src
