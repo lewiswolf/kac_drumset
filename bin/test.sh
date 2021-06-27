0db083174cfe33b96712f1a758a07d38b69f2d35
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-28 00:34:13 +0100

    added src to test

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
-pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py
+pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py src
 
 # arg is used to stop mypy getting annoyed about the src folder...
 # could/should probably be deleted in the future
-pipenv run mypy --ignore-missing-imports index.py 
+pipenv run mypy --ignore-missing-imports index.py src
--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,11 @@ This file is used to define and configure the project settings.
 
 from typing import TypedDict
 
+
 # type declarations
 class Settings(TypedDict):
-    test: int
+	test: int
+
 
 # the configurable object
 settings: Settings = {
