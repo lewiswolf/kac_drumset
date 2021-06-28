8cc74d0e81cac32b6006977887502bc4e56c7b4b
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-28 01:49:55 +0100

    made bin a test folder instead

--- bin/test.sh
+++
@@ -1,2 +0,0 @@
-pipenv run flake8 --config=setup.cfg index.py src
-pipenv run mypy --config-file=setup.cfg index.py src
---
+++ test/test.sh
@@ -0,0 +1,2 @@
+pipenv run flake8 --config=bin/test.cfg index.py src
+pipenv run mypy --config-file=bin/test.cfg index.py src
