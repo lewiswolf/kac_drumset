69e45e4a648485b36e1c15e85756e3e4c850be21
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-28 01:33:16 +0100

    moved test settings to setup.cfg

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,2 @@
-pipenv run flake8 --max-line-length 110 --ignore W191,E261,E402 index.py src
-
-# --ignore-missing-imports is used to stop mypy getting annoyed at dependencies..
-# could/should probably be deleted in the future (may be to do with pipenv)
-pipenv run mypy --ignore-missing-imports index.py src
+pipenv run flake8 index.py src
+pipenv run mypy index.py src
