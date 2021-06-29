9a0eb839a6adab8b7713747416b4e004e699827f
lewiswolf <lewiswolstanholme@gmail.com>
2021-06-29 22:32:48 +0100

    implicit variable typing is verbose

--- test/test_utils.py
+++ test/test_utils.py
@@ -17,20 +17,20 @@ class testTone():
 	'''
 
 	def __init__(self, hz: float, length: float, sr: int) -> None:
-		self.hz: float = hz											# frequency
-		self.sr = sr												# sample rate
-		self.length: int = math.ceil(self.sr * length)				# duration of sine wave in samples
-		self.wave: npt.NDArray[np.float64] = self.__generateWav()	# wave array
+		self.hz = hz											# frequency
+		self.sr = sr											# sample rate
+		self.length = math.ceil(self.sr * length)				# duration of sine wave in samples
+		self.wave = self.__generateWav()	# wave array
 
 	def __generateWav(self) -> npt.NDArray[np.float64]:
 		'''
 		Render a sine wave to a numpy array.
 		'''
 
-		two_pi: float = (2 * math.pi)
-		sampleLength: float = 1 / self.sr
-		phi: float = 0
-		wave: npt.NDArray[np.float64] = np.zeros(self.length)
+		two_pi = (2 * math.pi)
+		sampleLength = 1 / self.sr
+		phi = 0.0
+		wave = np.zeros(self.length)
 
 		for i in range(self.length):
 			wave[i] = math.sin(phi)
