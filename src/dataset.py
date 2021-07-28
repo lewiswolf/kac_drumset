cd85549d5d76bd4b6c35c7a05b761b65044e6fdb
lewiswolf <lewiswolstanholme@gmail.com>
2021-07-28 16:33:41 +0100

    more huge performance improvements on loadDataset()

--- src/dataset.py
+++ src/dataset.py
@@ -10,11 +10,10 @@ is not the case, the dataset is either ammended or regenerated entirely.
 import json
 import os
 import sys
-from typing import cast, Literal, Type, TypedDict
+from typing import Literal, TextIO, Type, TypedDict
 
 # dependencies
 import click					# CLI arguments
-import pydantic					# runtime type-checking
 import soundfile as sf			# audio read & write
 import torch					# pytorch
 from tqdm import tqdm			# CLI progress bar
@@ -25,14 +24,21 @@ from input_features import inputFeatures, inputSize
 from settings import settings, SpectroSettings
 
 
+tqdmSettings = {
+	'bar_format': '{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}, Elapsed: {elapsed}, ETA: {remaining}, {rate_fmt}  ',
+	'unit': ' data samples',
+	'total': settings['DATASET_SIZE'],
+}
+
+
 class SampleMetadata(TypedDict):
 	'''
 	Metadata format for each audio sample. Each audio sample consists of a wav file stored
 	on disk alongside its respective input data (x) and labels (y).
 	'''
-	filepath: str				# location of .wav file, relative to project directory
-	x: list						# input data for the network
-	y: list						# labels for each sample
+	filepath: str											# location of .wav file, relative to project directory
+	x: list													# input data for the network
+	y: list													# labels for each sample
 
 
 class DatasetMetadata(TypedDict):
@@ -71,6 +77,39 @@ class TorchDataset(torch.utils.data.Dataset):
 		return settings['DATASET_SIZE']
 
 
+def writeMetadataToFile(file: TextIO) -> None:
+	'''
+	Appends the project metadata, as defined in DatasetMetadata, to a .json file. The
+	file is intended to be opened in 'append' mode. 
+	'''
+
+	file.write(r'{' + f'{os.linesep}')
+	for key in DatasetMetadata.__dict__['__annotations__'].keys():
+		if key != 'data':
+			# TO FIX: see todo.md -> 'Extendable way to loop over TypedDict keys'
+			file.write(rf'"{key}": {json.dumps(settings[key])},{os.linesep}')
+	file.write(rf'"data": [{os.linesep}')
+
+
+def writeDataSampleToFile(file: TextIO, i: int, samplePath: str, x: torch.Tensor, y: torch.Tensor) -> None:
+	'''
+	Appends a datasample, as defined in SampleMetadata, to a .json file. The file is 
+	intended to be opened in 'append' mode, and this function is designed for use within 
+	a for loop.
+	'''
+
+	file.write(
+		r'{' + f'{os.linesep}'
+		+ fr'"filepath": "{samplePath}",{os.linesep}'
+		+ fr'"x": {x.tolist()},{os.linesep}'
+		+ fr'"y": {y.tolist()}{os.linesep}',
+	)
+	if i != settings['DATASET_SIZE'] - 1:
+		file.write(r'},' + f'{os.linesep}')
+	else:
+		file.write(r'}]}')
+
+
 def generateDataset(DataSample: Type[AudioSample]) -> TorchDataset:
 	'''
 	Generates a dataset of audio samples. The generated dataset, including the individual
@@ -79,32 +118,16 @@ def generateDataset(DataSample: Type[AudioSample]) -> TorchDataset:
 
 	cwd = os.getcwd()
 	dataset = TorchDataset()
-	metadata: DatasetMetadata = {
-		'DATASET_SIZE': settings['DATASET_SIZE'],
-		'DATA_LENGTH': settings['DATA_LENGTH'],
-		'SAMPLE_RATE': settings['SAMPLE_RATE'],
-		'INPUT_FEATURES': settings['INPUT_FEATURES'],
-		'NORMALISE_INPUT': settings['NORMALISE_INPUT'],
-		'SPECTRO_SETTINGS': settings['SPECTRO_SETTINGS'],
-		'data': [],
-	}
 
 	# clear dataset folder
 	for file in os.listdir(f'{cwd}/data'):
 		if file != '.gitignore':
 			os.remove(f'{cwd}/data/{file}')
 
-	# generate datatset
 	print('Generating dataset... 🎯')
-	with open(f'{cwd}/data/metadata.json', 'a') as jsonFile:
-		# add the initial section to metadata.json
-		jsonFile.write(json.dumps(metadata)[: -2])
-
-		with tqdm(
-			bar_format='{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}, Elapsed: {elapsed}, ETA: {remaining}, {rate_fmt}  ',
-			unit=' data samples',
-			total=settings['DATASET_SIZE'],
-		) as pbar:
+	with open(f'{cwd}/data/metadata.json', 'at') as newFile:
+		writeMetadataToFile(newFile)
+		with tqdm(**tqdmSettings) as pbar:
 			for i in range(settings['DATASET_SIZE']):
 				# prepare sample
 				sample = DataSample()
@@ -119,14 +142,9 @@ def generateDataset(DataSample: Type[AudioSample]) -> TorchDataset:
 				relativePath = f'data/sample_{i:05d}.wav'
 				sample.__export__(f'{cwd}/{relativePath}')
 				# export metadata
-				jsonFile.write(r'{' + fr'"filepath": "{relativePath}", "x": {x.tolist()}, "y": {sample.y}')
-				if i != settings['DATASET_SIZE'] - 1:
-					jsonFile.write(r'}, ')
-				else:
-					jsonFile.write(r'}]}')
-					jsonFile.close()
+				writeDataSampleToFile(newFile, i, relativePath, x, y)
 				pbar.update(1)
-
+		newFile.close()
 	return dataset
 
 
@@ -136,99 +154,92 @@ def loadDataset(DataSample: Type[AudioSample]) -> TorchDataset:
 	of the loaded dataset, and ammends the dataset if necessary.
 	'''
 
-	print('Preprocessing dataset... 📚')
 	cwd = os.getcwd()
 
-	# custom exceptions used to handle changes made to the settings
 	class DatasetIncompatible(Exception):
 		pass
 
-	class InputIncompatible(Exception):
-		pass
-
 	try:
-		# load a dataset if it exists
-		metadata: DatasetMetadata = json.load(open(f'{cwd}/data/metadata.json', 'r'))
-		# validate and enforce types at runtime
-		# TO FIX: see todo.md -> 'pydantic.create_model_from_typeddict has an incompatible type error'
-		metadata = cast(DatasetMetadata, pydantic.create_model_from_typeddict(DatasetMetadata)(**metadata).dict())
+		with open(f'{cwd}/data/metadata.json') as file:
+			# skip the initial '{'
+			file.readlines(1)
 
-		# confirm matching dataset settings
-		# TO ADD: see todo.md -> 'Extendable way to loop over TypedDict keys'
-		if (metadata['DATASET_SIZE'] < settings['DATASET_SIZE']
-					or [metadata['SAMPLE_RATE'], metadata['DATA_LENGTH']]
-					!= [settings['SAMPLE_RATE'], settings['DATA_LENGTH']]):
-			raise DatasetIncompatible
+			# confirm project settings match
+			inputIncompatible = False
+			for key in DatasetMetadata.__dict__['__annotations__'].keys():
+				if key == 'data':
+					file.readlines(1)
+					break
+				value = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+				# TO FIX: see todo.md -> 'Extendable way to loop over TypedDict keys'
+				if key == 'DATASET_SIZE' and settings[key] > value:
+					raise DatasetIncompatible
+				elif (key == 'SAMPLE_RATE' or key == 'DATA_LENGTH') and settings[key] != value:
+					raise DatasetIncompatible
+				elif settings[key] != value:
+					inputIncompatible = True
 
-		dataset = TorchDataset()
-		dataset.Y = torch.zeros(
-			(settings['DATASET_SIZE'],) + tuple(torch.as_tensor(metadata['data'][0]['y']).shape),
-		)
+			print('Preprocessing dataset... 📚')
+			dataset = TorchDataset()
 
-		# confirm matching input settings
-		# TO ADD: see todo.md -> 'Extendable way to loop over TypedDict keys'
-		if ([metadata['INPUT_FEATURES'], metadata['NORMALISE_INPUT'], metadata['SPECTRO_SETTINGS']]
-					!= [settings['INPUT_FEATURES'], settings['NORMALISE_INPUT'], settings['SPECTRO_SETTINGS']]):
-			raise InputIncompatible
+			# construct dataset from json
+			if not inputIncompatible:
+				with tqdm(**tqdmSettings) as pbar:
+					for i in range(settings['DATASET_SIZE']):
+						# import relevant information
+						file.readlines(2)
+						x = torch.as_tensor(
+							json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1]),
+						)
+						y = torch.as_tensor(
+							json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:]),
+						)
+						file.readlines(1)
+						# on the initial run, infer the size of dataset.Y
+						if i == 0:
+							dataset.Y = torch.zeros((settings['DATASET_SIZE'],) + tuple(y.shape))
+						# append input features to dataset
+						dataset.setitem(i, x, y)
+						if i == settings['DATASET_SIZE'] - 1:
+							file.close()
+						pbar.update(1)
 
-	# if no dataset exists
-	except FileNotFoundError:
-		print('Could not load a dataset. 🤷')
-		if not click.confirm('Do you want to generate a new dataset?', default=None, prompt_suffix=': '):
-			sys.exit()
-		return generateDataset(DataSample)
-
-	# if the dataset is incompatible with the current project settings
-	except (pydantic.ValidationError, DatasetIncompatible):
-		print('Imported dataset is incompatible with the current project settings. 🤷')
-		if not click.confirm('Do you want to generate a new dataset?', default=None, prompt_suffix=': '):
-			print('Check data/metadata.json to see your previous project setting.')
-			sys.exit()
-		return generateDataset(DataSample)
-
-	# recompute input features if project settings don't align
-	except InputIncompatible:
-		# initialise new metadata.json
-		os.remove(f'{cwd}/data/metadata.json')
-		with open(f'{cwd}/data/metadata.json', 'a') as jsonFile:
-			jsonFile.write(json.dumps({
-				'DATASET_SIZE': settings['DATASET_SIZE'],
-				'DATA_LENGTH': settings['DATA_LENGTH'],
-				'SAMPLE_RATE': settings['SAMPLE_RATE'],
-				'INPUT_FEATURES': settings['INPUT_FEATURES'],
-				'NORMALISE_INPUT': settings['NORMALISE_INPUT'],
-				'SPECTRO_SETTINGS': settings['SPECTRO_SETTINGS'],
-				'data': [],
-			})[: -2])
-
-			with tqdm(
-				bar_format='{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}, Elapsed: {elapsed}, ETA: {remaining}, {rate_fmt}  ',
-				unit=' data samples',
-				total=settings['DATASET_SIZE'],
-			) as pbar:
-				for i in range(settings['DATASET_SIZE']):
-					# generate new input features
-					relativePath = metadata['data'][i]['filepath']
-					x = inputFeatures(sf.read(f'{cwd}/{relativePath}')[0])
-					y = metadata['data'][i]['y']
-					dataset.setitem(i, x, torch.as_tensor(y))
-
-					# export metadata
-					jsonFile.write(r'{' + fr'"filepath": "{relativePath}", "x": {x.tolist()}, "y": {y}')
-					if i != settings['DATASET_SIZE'] - 1:
-						jsonFile.write(r'}, ')
-					else:
-						jsonFile.write(r'}]}')
-						jsonFile.close()
-					pbar.update(1)
-
+			# regenerate inputs features and metadata.json
+			else:
+				with open(f'{cwd}/data/metadata_temp.json', 'at') as newFile:
+					writeMetadataToFile(newFile)
+					with tqdm(**tqdmSettings) as pbar:
+						for i in range(settings['DATASET_SIZE']):
+							# import relevant information
+							file.readlines(1)
+							relativePath = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+							file.readlines(1)
+							x = inputFeatures(sf.read(f'{cwd}/{relativePath}')[0])
+							y = torch.as_tensor(
+								json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:]),
+							)
+							file.readlines(1)
+							# on the initial run, infer the size of dataset.Y
+							if i == 0:
+								dataset.Y = torch.zeros((settings['DATASET_SIZE'],) + tuple(y.shape))
+							# append input features to dataset
+							dataset.setitem(i, x, y)
+							# export metadata
+							writeDataSampleToFile(newFile, i, relativePath, x, y)
+							pbar.update(1)
+					file.close()
+					os.remove(f'{cwd}/data/metadata.json')
+					os.rename(f'{cwd}/data/metadata_temp.json', f'{cwd}/data/metadata.json')
+					newFile.close()
 		return dataset
 
-	# generate torch dataset if all checks are passed
-	for i in range(settings['DATASET_SIZE']):
-		dataset.setitem(
-			i,
-			torch.as_tensor(metadata['data'][i]['x']),
-			torch.as_tensor(metadata['data'][i]['y']),
-		)
-	return dataset
+	# handle exceptions
+	except Exception as e:
+		if type(e).__name__ == 'DatasetIncompatible':
+			print('Imported dataset is incompatible with the current project settings. 🤷')
+			print('Check data/metadata.json to see your previous project setting.')
+		else:
+			print('Could not load a dataset. 🤷')
+		if not click.confirm('Do you want to generate a new dataset?', default=None, prompt_suffix=': '):
+			sys.exit()
+		return generateDataset(DataSample)
