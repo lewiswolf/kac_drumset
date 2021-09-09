8b1223ebf0ba540317a247718ff8d6ece14b4a83
lewiswolf <lewiswolstanholme@gmail.com>
2021-09-09 23:00:41 +0100

    overhaul of input features, with performance upgrades

--- src/input_features.py
+++ src/input_features.py
@@ -1,11 +1,12 @@
 '''
 This file is used to transform arrays of raw audio into various input types
 ready for training a neural network. This file also includes other helper
-methods relating to the size of the input data.
+methods relating to the size of the input data and normalising audio.
 '''
 
 # core
 import math
+from typing import Any, Literal
 
 # dependencies
 import numpy as np 				# maths
@@ -16,77 +17,155 @@ import torchaudio				# tensor audio manipulation
 
 # src
 from settings import SpectroSettings, settings
-specSettings: SpectroSettings = settings['SPECTRO_SETTINGS']
+spec_settings: SpectroSettings = settings['spectro_settings']
 
 
-# config settings specifically for use with a constant q transform
-if settings['INPUT_FEATURES'] == 'cqt':
-	f_min = 22.05
-	max_octaves = math.log2((settings['SAMPLE_RATE'] * 0.5) / f_min)
-	bins_per_octave = math.floor(specSettings['n_bins'] / max_octaves)
-	n_bins = math.floor(bins_per_octave * max_octaves)
-
-
-def inputSize() -> tuple[int, ...]:
+class InputFeatures():
 	'''
-	Helper method used for precomputing the size of an individual input feature.
+	This class is used to convert a raw waveform into a user defined input
+	representation, which includes the fourier transform, mel spectrogram
+	and constant q transform. The intended use of this class when deployed:
+		IF = InputFeatures()
+		X = np.zeros((n,) + IF.transformShape(len(waveform))))
+		for i in range(n):
+			X[i] = IF.transform(waveform)
 	'''
 
-	if settings['INPUT_FEATURES'] == 'end2end':
-		return (math.ceil(settings['DATA_LENGTH'] * settings['SAMPLE_RATE']), )
-	else:
-		temporalWidth = math.ceil(
-			(math.ceil(settings['DATA_LENGTH'] * settings['SAMPLE_RATE']) + 1) / specSettings['hop_length'],
-		)
-		if settings['INPUT_FEATURES'] == 'fft':
-			return (specSettings['n_bins'] // 2 + 1, temporalWidth)
-		if settings['INPUT_FEATURES'] == 'mel':
-			return (specSettings['n_mels'], temporalWidth)
-		if settings['INPUT_FEATURES'] == 'cqt':
-			return (n_bins, temporalWidth)
+	bins_per_octave: int
+	f_min: float
+	feature_type: Literal['end2end', 'fft', 'mel', 'cqt']
+	hop_length: int
+	n_bins: int
+	n_mels: int
+	normalise_input: bool
+	sr: int
+	# for an explanation of why this type is Any, see
+	# todo.md => `Assigning class methods to class variables`.
+	# transform: Callable[[Any, npt.NDArray[np.float64]], torch.Tensor]
+	transform: Any
+	transformer: torch.nn.Module
+	window_length: int
 
+	def __init__(
+		self,
+		feature_type: Literal['end2end', 'fft', 'mel', 'cqt'] = settings['input_features'],
+		normalise_input: bool = settings['normalise_input'],
+		sr: int = settings['sample_rate'],
+		f_min: float = spec_settings['f_min'],
+		hop_length: int = spec_settings['hop_length'],
+		n_bins: int = spec_settings['n_bins'],
+		n_mels: int = spec_settings['n_mels'],
+		window_length: int = spec_settings['window_length'],
+	) -> None:
+		'''
+		This method sets up the class ready to produce input features. Based on the type
+		of output/spectrogram, the internal settings are also configured.
+		'''
 
-def inputFeatures(waveform: npt.NDArray[np.float64]) -> torch.Tensor:
-	'''
-	This method produces the input features to pass to the neural network, by
-	first importing a pre-generated .wav file, returning a tensor corresponding
-	to a range of input features.
-	'''
+		# initialise user defined variables
+		self.feature_type = feature_type
+		self.normalise_input = normalise_input
+		self.sr = sr
 
-	# normalise the audio file
-	if settings['NORMALISE_INPUT'] and np.max(waveform) != 1.0:
-		waveform = waveform * (1.0 / np.max(waveform))
+		# configure end2end
+		if self.feature_type == 'end2end':
+			self.transform = self.__end2end__
 
-	# return the correct input representation as a tensor
-	if settings['INPUT_FEATURES'] == 'end2end':
-		return torch.as_tensor(waveform)
+		# configure fft
+		if self.feature_type == 'fft':
+			self.hop_length = hop_length
+			self.n_bins = n_bins
+			self.window_length = window_length
+			self.transformer = torchaudio.transforms.Spectrogram(
+				hop_length=self.hop_length,
+				n_fft=self.n_bins,
+				power=2.0,
+				win_length=self.window_length,
+			)
+			self.transform = self.__withTransformer__
 
-	if settings['INPUT_FEATURES'] == 'fft':
-		return torchaudio.transforms.Spectrogram(
-			n_fft=specSettings['n_bins'],
-			win_length=specSettings['window_length'],
-			hop_length=specSettings['hop_length'],
-			power=2.0,
-		)(torch.as_tensor(waveform))
+		# configure mel
+		if self.feature_type == 'mel':
+			# Necessary to fix a bug with mel spectrograms, see todo.md ->
+			# 'Internal types for nested lists, numpy arrays and pytroch tensors'
+			torch.set_default_dtype(torch.float64)
+			self.f_min = f_min
+			self.hop_length = hop_length
+			self.n_bins = n_bins
+			self.n_mels = n_mels
+			self.window_length = window_length
+			self.transformer = torchaudio.transforms.MelSpectrogram(
+				hop_length=self.hop_length,
+				n_fft=self.n_bins,
+				n_mels=self.n_mels,
+				power=2.0,
+				sample_rate=self.sr,
+				win_length=self.window_length,
+			)
+			self.transform = self.__withTransformer__
 
-	if settings['INPUT_FEATURES'] == 'mel':
-		return torchaudio.transforms.MelSpectrogram(
-			sample_rate=settings['SAMPLE_RATE'],
-			n_mels=specSettings['n_mels'],
-			n_fft=specSettings['n_bins'],
-			win_length=specSettings['window_length'],
-			hop_length=specSettings['hop_length'],
-			power=2.0,
-		)(torch.as_tensor(waveform))
+		# configure cqt
+		if self.feature_type == 'cqt':
+			self.f_min = f_min
+			self.hop_length = hop_length
+			# recalculate n_bins to form the closest match to the original kwarg
+			max_octaves = math.log2((self.sr * 0.5) / self.f_min)
+			self.bins_per_octave = math.floor(n_bins / max_octaves)
+			self.n_bins = math.floor(self.bins_per_octave * max_octaves)
+			self.transform = self.__cqt__
 
-	if settings['INPUT_FEATURES'] == 'cqt':
-		# TO ADD: see todo.md -> 'Port librosa.vqt() to PyTorch'
+	def __cqt__(self, waveform: npt.NDArray[np.float64]) -> torch.Tensor:
+		'''
+		Calcualte the constant q transform using librosa.
+		TO ADD: see todo.md -> 'Port librosa.vqt() to PyTorch'.
+		'''
+		waveform = self.__normalise__(waveform) if self.normalise_input else waveform
 		return torch.as_tensor(np.abs(librosa.cqt(
-			waveform,
-			sr=settings['SAMPLE_RATE'],
-			n_bins=n_bins,
-			bins_per_octave=bins_per_octave,
-			fmin=f_min,
-			hop_length=specSettings['hop_length'],
+			self.__normalise__(waveform),
+			sr=self.sr,
+			n_bins=self.n_bins,
+			bins_per_octave=self.bins_per_octave,
+			fmin=self.f_min,
+			hop_length=self.hop_length,
 			dtype=np.float64,
 		)))
+
+	def __end2end__(self, waveform: npt.NDArray[np.float64]) -> torch.Tensor:
+		'''
+		Convert a numpy array into a pytorch tensor.
+		'''
+		waveform = self.__normalise__(waveform) if self.normalise_input else waveform
+		return torch.as_tensor(waveform)
+
+	@staticmethod
+	def __normalise__(waveform: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+		'''
+		Normalise an audio waveform, such that x ∈ [-1.0, 1.0]
+		'''
+		x_min = np.min(waveform)
+		return 2.0 * (waveform - x_min) / (np.max(waveform) - x_min) - 1.0
+
+	def __withTransformer__(self, waveform: npt.NDArray[np.float64]) -> torch.Tensor:
+		'''
+		Calculate a spectrogram output using a transformer (torch.nn.Module).
+		'''
+		waveform = self.__normalise__(waveform) if self.normalise_input else waveform
+		return self.transformer(torch.as_tensor(waveform))
+
+	def transformShape(self, data_length: int) -> tuple[int, ...]:
+		'''
+		Helper method used for precomputing the shape of an individual input feature.
+		params:
+			data_length		Length of the audio file (samples).
+		'''
+
+		if self.feature_type == 'end2end':
+			return (data_length, )
+		else:
+			temporalWidth = math.ceil((data_length + 1) / self.hop_length)
+			if self.feature_type == 'fft':
+				return (self.n_bins // 2 + 1, temporalWidth)
+			if self.feature_type == 'mel':
+				return (self.n_mels, temporalWidth)
+			if self.feature_type == 'cqt':
+				return (self.n_bins, temporalWidth)
