ac3b3c3971794f762730198a116fc200a43ae9f9
lewiswolf <lewiswolstanholme@gmail.com>
2021-11-10 09:55:01 +0000

    geometry overhaul, reorganisation and future plans

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -4,136 +4,209 @@ tests is used in conjunction with a particular project file.
 '''
 
 # core
-import json
+# import json
 import os
-import random
-import shutil
+# import random
+# import shutil
 import sys
-from typing import cast
+# from typing import cast
 import unittest
 
 # dependencies
 import cv2					# image processing
 import numpy as np 			# maths
-import pydantic 			# runtime type-checking
+# import pydantic 			# runtime type-checking
 import torch				# pytorch
 
 # src
 sys.path.insert(1, f'{os.getcwd()}/src')
-import dataset as ds
-from geometry import RandomPolygon, isConvex, isColinear, largestVector
+# import dataset as ds
+from geometry import isConvex, isColinear, largestVector
 from input_features import InputFeatures
-from physical_model import DrumModel, raisedCosine
+# from physical_model import DrumModel, raisedCosine
+from random_polygon import RandomPolygon
+# from settings import settings
 
 # test
-from test_utils import TestSweep, TestTone, noPrinting
+from test_utils import TestSweep, noPrinting
+# from test_utils import TestTone
 
 
-class DatasetTests(unittest.TestCase):
-	'''
-	Tests used in conjunction with `dataset.py`.
-	'''
+# class DatasetTests(unittest.TestCase):
+# '''
+# Tests used in conjunction with `dataset.py`.
+# '''
 
-	@classmethod
-	def tearDownClass(cls) -> None:
-		'''
-		Clear the /tmp folder after all tests are done.
-		Maybe move this...
-		'''
+# @classmethod
+# def tearDownClass(cls) -> None:
+# 	'''
+# 	Clear the /tmp folder after all tests are done.
+# 	Maybe move this...
+# 	'''
 
-		directory = f'{os.getcwd()}/test/tmp'
-		for file in os.listdir(directory):
-			path = f'{directory}/{file}'
-			if os.path.isdir(path):
-				shutil.rmtree(path)
-			elif file != '.gitignore':
-				os.remove(path)
+# 	directory = f'{os.getcwd()}/test/tmp'
+# 	for file in os.listdir(directory):
+# 		path = f'{directory}/{file}'
+# 		if os.path.isdir(path):
+# 			shutil.rmtree(path)
+# 		elif file != '.gitignore':
+# 			os.remove(path)
 
-	def test_metadata_stringify(self) -> None:
-		'''
-		First stringifies the dataset's metadata, ready for exporting a .json file, and
-		then checks that it containes the correct values and data types.
-		'''
+# def test_dataset_settings_confirmation(self) -> None:
+# 	'''
+# 	When a dataset is loaded, the dataset metadata is checked alongside the current
+# 	project settings to look for changes. The tests below are used to confirm that
+# 	these changes are correctly identified.
+# 	'''
+# 	# create an initial dataset with current project settings
+# 	dataset_dir = f'{os.getcwd()}/test/tmp'
+# 	settings_copy = settings.copy()
+# 	settings_copy['dataset_size'] = 10
+# 	ds.generateDataset(TestTone, dataset_size=10, dataset_dir='test/tmp')
 
-		# number of tests
-		n = 10
+# 	# This test asserts that the initial configuration loads correctly
+# 	with open(f'{dataset_dir}/metadata.json') as file:
+# 		ds.confirmDatasetSettings(file, {}, s=settings_copy)
+# 		file.close()
 
-		# Test with y labels.
-		str = ds.parseMetadataToString(
-			sampler_settings={
-				'key1': 'value1',
-				'key2': 'value2',
-			},
-		)
-		for i in range(n):
-			str += ds.parseDataSampleToString({'filepath': '', 'x': [], 'y': [1]}, i == n - 1)
-		JSON = json.loads(str)
-		# This test asserts that the y labels exist.
-		for i in range(n):
-			self.assertTrue('y' in JSON['data'][i])
-		# This test asserts that the dataset matches the type specification.
-		cast(
-			ds.DatasetMetadata,
-			pydantic.create_model_from_typeddict(ds.DatasetMetadata)(**JSON).dict(),
-		)
+# 	# These tests assert that the DatasetIncompatible error is raised appropriately.
+# 	for i in range(4):
+# 		sampler_settings = {}
+# 		if i == 0:
+# 			settings_copy.update({
+# 				'dataset_size': 11,
+# 			})
+# 		if i == 1:
+# 			settings_copy.update({
+# 				'data_length': 0.0,
+# 				'dataset_size': 10,
+# 			})
+# 		if i == 2:
+# 			settings_copy.update({
+# 				'data_length': settings['data_length'],
+# 				'sample_rate': 0,
+# 			})
+# 		if i == 3:
+# 			settings_copy.update({
+# 				'sample_rate': settings['sample_rate'],
+# 			})
+# 			sampler_settings = {'some_key': 'some_val'}
 
-		# Test with falsey y labels.
-		str = ds.parseMetadataToString(
-			sampler_settings={
-				'key1': 'value1',
-				'key2': 'value2',
-			},
-		)
-		for i in range(n):
-			if random.getrandbits(1):
-				str += ds.parseDataSampleToString({'filepath': '', 'x': []}, i == n - 1)
-			else:
-				str += ds.parseDataSampleToString({'filepath': '', 'x': [], 'y': []}, i == n - 1)
-		JSON = json.loads(str)
-		# This test asserts that the y labels do not exist.
-		for i in range(n):
-			self.assertFalse('y' in JSON['data'][i])
-		# This test asserts that the dataset matches the type specification.
-		cast(
-			ds.DatasetMetadata,
-			pydantic.create_model_from_typeddict(ds.DatasetMetadata)(**JSON).dict(),
-		)
+# 		with open(f'{dataset_dir}/metadata.json') as file:
+# 			self.assertRaises(
+# 				ds.DatasetIncompatible,
+# 				ds.confirmDatasetSettings,
+# 				file,
+# 				sampler_settings,
+# 				s=settings_copy,
+# 			)
+# 			file.close()
 
-	def test_generated_dataset(self) -> None:
-		'''
-		Tests associated with generating a dataset. These test check for the correct size
-		and data type of the dataset, both in memory and on disk.
-		'''
+# 	# These tests assert that the InputIncompatible error is raised appropriately.
+# 	# After raising this error, the file is also checked to be at the appropriate
+# 	# read location.
+# 	for i in range(1):
+# 		if i == 0:
+# 			settings_copy.update({
+# 				'input_features': 'fft' if settings['input_features'] == 'end2end' else 'end2end',
+# 			})
 
-		# Test with y.
-		with noPrinting():
-			dataset = ds.generateDataset(TestTone, dataset_size=10, dataset_dir='test/tmp')
+# 		with open(f'{dataset_dir}/metadata.json') as file:
+# 			self.assertRaises(
+# 				ds.InputIncompatible,
+# 				ds.confirmDatasetSettings,
+# 				file,
+# 				{},
+# 				s=settings_copy,
+# 			)
+# 			self.assertEqual(file.readlines(1), ['{\n'])
+# 			file.close()
 
-		# This test asserts that the dataset is the correct size, both in memory and on disk.
-		self.assertEqual(dataset.__len__(), 10)
-		self.assertEqual(len(dataset.Y), 10)
-		self.assertEqual(len(os.listdir(f'{os.getcwd()}/test/tmp')) - 2, 10)
+# def test_generated_dataset(self) -> None:
+# 	'''
+# 	Tests associated with generating a dataset. These test check for the correct size
+# 	and data type of the dataset, both in memory and on disk.
+# 	'''
 
-		# This test asserts that x and y are the correct data types.
-		for i in range(10):
-			x, y = dataset.__getitem__(i)
-			self.assertEqual(x.dtype, torch.float64)
-			self.assertNotEqual(y, None)
-			if y: # for mypy only
-				self.assertEqual(y.dtype, torch.float64)
+# 	# Test with y.
+# 	with noPrinting():
+# 		dataset = ds.generateDataset(TestTone, dataset_size=10, dataset_dir='test/tmp')
 
-		# Test without y.
-		with noPrinting():
-			dataset = ds.generateDataset(TestSweep, dataset_size=10, dataset_dir='test/tmp')
+# 	# This test asserts that the dataset is the correct size, both in memory and on disk.
+# 	self.assertEqual(dataset.__len__(), 10)
+# 	self.assertEqual(len(dataset.Y), 10)
+# 	self.assertEqual(len(os.listdir(f'{os.getcwd()}/test/tmp')) - 2, 10)
 
-		# This test asserts that dataset.Y does not exist.
-		self.assertFalse(hasattr(dataset, 'Y'))
+# 	# This test asserts that x and y are the correct data types.
+# 	for i in range(10):
+# 		x, y = dataset.__getitem__(i)
+# 		self.assertEqual(x.dtype, torch.float64)
+# 		self.assertNotEqual(y, None)
+# 		if y: # for mypy only
+# 			self.assertEqual(y.dtype, torch.float64)
 
-		# This test asserts that x and y are the correct data types.
-		for i in range(10):
-			x, y = dataset.__getitem__(i)
-			self.assertEqual(x.dtype, torch.float64)
-			self.assertEqual(y, None)
+# 	# Test without y.
+# 	with noPrinting():
+# 		dataset = ds.generateDataset(TestSweep, dataset_size=10, dataset_dir='test/tmp')
+
+# 	# This test asserts that dataset.Y does not exist.
+# 	self.assertFalse(hasattr(dataset, 'Y'))
+
+# 	# This test asserts that x and y are the correct data types.
+# 	for i in range(10):
+# 		x, y = dataset.__getitem__(i)
+# 		self.assertEqual(x.dtype, torch.float64)
+# 		self.assertEqual(y, None)
+
+# def test_metadata_stringify(self) -> None:
+# 	'''
+# 	First stringifies the dataset's metadata, ready for exporting a .json file, and
+# 	then checks that it containes the correct values and data types.
+# 	'''
+
+# 	# number of tests
+# 	n = 10
+
+# 	# Test with y labels.
+# 	str = ds.parseMetadataToString(
+# 		sampler_settings={
+# 			'key1': 'value1',
+# 			'key2': 'value2',
+# 		},
+# 	)
+# 	for i in range(n):
+# 		str += ds.parseDataSampleToString({'filepath': '', 'x': [], 'y': [1]}, i == n - 1)
+# 	JSON = json.loads(str)
+# 	# This test asserts that the y labels exist.
+# 	for i in range(n):
+# 		self.assertTrue('y' in JSON['data'][i])
+# 	# This test asserts that the dataset matches the type specification.
+# 	cast(
+# 		ds.DatasetMetadata,
+# 		pydantic.create_model_from_typeddict(ds.DatasetMetadata)(**JSON).dict(),
+# 	)
+
+# 	# Test with falsey y labels.
+# 	str = ds.parseMetadataToString(
+# 		sampler_settings={
+# 			'key1': 'value1',
+# 			'key2': 'value2',
+# 		},
+# 	)
+# 	for i in range(n):
+# 		if random.getrandbits(1):
+# 			str += ds.parseDataSampleToString({'filepath': '', 'x': []}, i == n - 1)
+# 		else:
+# 			str += ds.parseDataSampleToString({'filepath': '', 'x': [], 'y': []}, i == n - 1)
+# 	JSON = json.loads(str)
+# 	# This test asserts that the y labels do not exist.
+# 	for i in range(n):
+# 		self.assertFalse('y' in JSON['data'][i])
+# 	# This test asserts that the dataset matches the type specification.
+# 	cast(
+# 		ds.DatasetMetadata,
+# 		pydantic.create_model_from_typeddict(ds.DatasetMetadata)(**JSON).dict(),
+# 	)
 
 
 class GeometryTests(unittest.TestCase):
@@ -178,7 +251,7 @@ class GeometryTests(unittest.TestCase):
 				# This test asserts that all supposedly convex polygons are in fact convex.
 				# As a result, if this test passes, we can assume that the generateConvex()
 				# function works as intended.
-				self.assertTrue(isConvex(polygon.n, polygon.vertices))
+				self.assertTrue(isConvex(polygon.vertices))
 
 				# This test asserts that the calculated centroid lies within the polygon. For
 				# concave shapes, this test may fail.
@@ -188,7 +261,7 @@ class GeometryTests(unittest.TestCase):
 				], 1)
 
 				# This test asserts that the largest vector is of magnitude 1.0.
-				self.assertEqual(largestVector(polygon.n, polygon.vertices)[0], 1.0)
+				self.assertEqual(largestVector(polygon.vertices)[0], 1.0)
 
 
 class InputFeatureTests(unittest.TestCase):
@@ -239,51 +312,71 @@ class InputFeatureTests(unittest.TestCase):
 		self.assertEqual(np.min(norm), -1.0)
 
 
-class PhysicalModelTests(unittest.TestCase):
-	'''
-	Tests used in conjunction with `physical_model.py`.
-	'''
+# class PhysicalModelTests(unittest.TestCase):
+# 	'''
+# 	Tests used in conjunction with `physical_model.py`.
+# 	'''
 
-	drum = DrumModel()
+# 	# drum = DrumModel()
 
-	def test_CFL_stability(self) -> None:
-		'''
-		The Courant number λ = γk/h is used to assert that the CFL stability criterion is upheld.
-		If λ > 1, the resultant simulation will be unstable.
-		'''
-		# For a 1D simulation
-		# self.assertLessEqual(self.drum.cfl, 1.0)
-		# For a 2D simulation
-		self.assertLessEqual(self.drum.cfl, 1 / (2 ** 0.5))
+# 	def test_CFL_stability(self) -> None:
+# 		'''
+# 		The Courant number λ = γk/h is used to assert that the CFL stability criterion is upheld.
+# 		If λ > 1, the resultant simulation will be unstable.
+# 		'''
+# 		# For a 1D simulation
+# 		# self.assertLessEqual(self.drum.cfl, 1.0)
+# 		# For a 2D simulation
 
-	def test_energy_conservation(self) -> None:
-		'''
-		For an accurate physical simulation, the conservation law of energy must be upheld. This
-		is both naively tested, using the waveform itself, and by comparing expected bounds on
-		the Hamiltonian energy throughout the simulation.
-		'''
+# 		drum_size = [0.9, 0.7, 0.5, 0.3, 0.1]
+# 		material_density = [0.75, 0.5, 0.25, 0.125, 0.0625]
+# 		tension = [3000.0, 2000.0, 1500.0, 1000.0]
+# 		for i in range(len(drum_size)):
+# 			for j in range(len(material_density)):
+# 				for k in range(len(tension)):
+# 					drum = DrumModel(
+# 						allow_concave=True,
+# 						decay_time=1.0,
+# 						max_vertices=10,
+# 						drum_size=drum_size[i],
+# 						material_density=material_density[j],
+# 						tension=tension[k],
+# 					)
+# 					self.assertLessEqual(drum.cfl, 1 / (2 ** 0.5))
+# 					drum.length = 1000 # very short simulation
+# 					drum.generateWaveform()
+# 					self.assertLessEqual(np.max(drum.waveform), 1.0)
+# 					self.assertGreaterEqual(np.min(drum.waveform), -1.0)
 
-		# This test asserts that the resultant waveform is always bounded betweenn 1.0 and -1.0.
-		self.drum.length = 1000 # very short simulation
-		self.drum.generateWaveform()
-		self.assertLessEqual(np.max(self.drum.waveform), 1.0)
-		self.assertGreaterEqual(np.min(self.drum.waveform), -1.0)
 
-	def test_raised_cosine(self) -> None:
-		'''
-		The raised cosine transform is used as the activation function for a physical model. These
-		tests assert that the raised cosine works as intended, both in the 1 and 2 dimensional case.
-		'''
+# 	# def test_energy_conservation(self) -> None:
+# 	# 	'''
+# 	# 	For an accurate physical simulation, the conservation law of energy must be upheld. This
+# 	# 	is both naively tested, using the waveform itself, and by comparing expected bounds on
+# 	# 	the Hamiltonian energy throughout the simulation.
+# 	# 	'''
 
-		# This test asserts that the one dimensional case has the correct peaks.
-		rc = raisedCosine((100, ), (50, ), sigma=10)
-		self.assertEqual(np.max(rc), 1.0)
-		self.assertEqual(np.min(rc), 0.0)
+# 	# 	# This test asserts that the resultant waveform is always bounded betweenn 1.0 and -1.0.
+# 	# 	self.drum.length = 1000 # very short simulation
+# 	# 	self.drum.generateWaveform()
+# 	# 	self.assertLessEqual(np.max(self.drum.waveform), 1.0)
+# 	# 	self.assertGreaterEqual(np.min(self.drum.waveform), -1.0)
 
-		# This test asserts that the two dimensional case has the correct peaks.
-		rc = raisedCosine((100, 100), (50, 50), sigma=10)
-		self.assertEqual(np.max(rc), 1.0)
-		self.assertEqual(np.min(rc), 0.0)
+# 	def test_raised_cosine(self) -> None:
+# 		'''
+# 		The raised cosine transform is used as the activation function for a physical model. These
+# 		tests assert that the raised cosine works as intended, both in the 1 and 2 dimensional case.
+# 		'''
+
+# 		# This test asserts that the one dimensional case has the correct peaks.
+# 		rc = raisedCosine((100, ), (50, ), sigma=10)
+# 		self.assertEqual(np.max(rc), 1.0)
+# 		self.assertEqual(np.min(rc), 0.0)
+
+# 		# This test asserts that the two dimensional case has the correct peaks.
+# 		rc = raisedCosine((100, 100), (50, 50), sigma=10)
+# 		self.assertEqual(np.max(rc), 1.0)
+# 		self.assertEqual(np.min(rc), 0.0)
 
 
 if __name__ == '__main__':
