6d97fb79a20f68b85781c1e9694e31b63971e455
lewiswolf <lewiswolstanholme@gmail.com>
2022-04-01 00:44:34 +0100

    missed audio_sampler

---
+++ kac_drumset/sampler/audio_sampler.py
@@ -0,0 +1,77 @@
+# core
+from abc import ABC, abstractmethod
+import math
+import wave
+from typing import Union
+
+# dependencies
+import numpy as np				# maths
+import numpy.typing as npt		# typing for numpy
+
+__all__ = [
+	'AudioSampler',
+]
+
+
+class AudioSampler(ABC):
+	'''
+	Template parent class for an audio sampler. The intended use when deployed:
+		sampler = AudioSampler()
+		for i in range(n):
+			sampler.updateParameters()
+			sampler.generateWaveform()
+			x = sampler.waveform
+			y = sampler.getLabels()
+			sampler.export()
+	'''
+
+	duration: float						# duration of the audio file (seconds)
+	sr: int								# sample rate (hz)
+	length: int							# length of the audio file (samples)
+	waveform: npt.NDArray[np.float64]	# the audio sample itself
+
+	def __init__(self, duration: float, sr: int) -> None:
+		'''
+		Initialise sampler.
+		'''
+		self.duration = duration
+		self.sr = sr
+		self.length = math.ceil(duration * sr)
+		self.waveform = np.zeros(self.length)
+
+	def export(self, absolutePath: str) -> None:
+		'''
+		Write the generated waveform to a file.
+		'''
+		with wave.open(absolutePath, 'w') as wav:
+			wav.setnchannels(1)
+			wav.setsampwidth(3)
+			wav.setframerate(self.sr)
+			wav.writeframes((self.waveform * (2 ** 23 - 1)).astype("<h").tobytes())
+
+	@abstractmethod
+	def generateWaveform(self) -> None:
+		'''
+		This method should be used to set self.waveform.
+		'''
+		pass
+
+	@abstractmethod
+	def getLabels(self) -> list[Union[float, int]]:
+		'''
+		This method should return the y labels for the generated audio.
+		'''
+		pass
+
+	# @abstractmethod
+	# def updateProperties(self) -> None:
+	# 	'''
+	# 	This method should be used to update the properties of the sampler
+	# 	when inside a generator loop.
+	# 	'''
+	# 	pass
+
+	# private
+	# self._sample_count = 0
+	# private
+	# _sample_count: int					# internal counter to track the amount of samples generated
