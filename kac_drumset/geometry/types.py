e04e713349332c993da485c093355617a0a53837
lewiswolf <lewiswolstanholme@gmail.com>
2023-01-09 19:24:35 +0000

    docs

--- kac_drumset/geometry/types.py
+++ kac_drumset/geometry/types.py
@@ -77,7 +77,7 @@ class Polygon(Shape):
 		'''
 		Tests whether or not a given polygon is convex. This is achieved using the resultant sign of the cross product for
 		each vertex:
-			[(x_i - x_i-1), (y_i - y_i-1)] x [(x_i+1 - x_i), (y_i+1 - y_i)].
+			[(x_i - x_i-1), (y_i - y_i-1)] × [(x_i+1 - x_i), (y_i+1 - y_i)].
 		See => http://paulbourke.net/geometry/polygonmesh/ 'Determining whether or not a polygon (2D) has its vertices ordered
 		clockwise or counter-clockwise'.
 		'''
