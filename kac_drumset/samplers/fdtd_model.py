fixed exploding gradient and changed np.min(a) to a.min() and np.max(a) to a.max()

kac_drumset/samplers/fdtd_model.py
@@ -76,7 +76,7 @@ class FDTDModel(AudioSampler):
 		drum_size: float = 0.3,
 		material_density: float = 0.2,
 		max_vertices: int = 10,
-		strike_width: float = 0.005,
+		strike_width: float = 0.01,
 		tension: float = 2000.,
 	) -> None:
 		'''
@@ -100,7 +100,7 @@ class FDTDModel(AudioSampler):
 		self.h = 1 / self.H
 		self.cfl = self.gamma * self.k / self.h
 		self.sigma = self.H * strike_width / self.L
-		self.sigma_2 = self.sigma ** 2.
+		self.sigma_2 = max(self.sigma ** 2., 1.)
 		# FDTD update coefficients
 		log_decay = self.k * 6 * np.log(10) / self.d_60
 		self.c_0 = (self.cfl ** 2) / (1 + log_decay)

kac_drumset/samplers/poisson_model.py
@@ -99,7 +99,7 @@ class PoissonModel(AudioSampler):
 			omega *= 2 * np.pi * self.k # rate of phase
 			for i in range(self.length):
 				# 2009 - Bilbao , pp.65-66
-				self.waveform[i] = np.sum(A * np.exp(i * self.decay) * np.sin(i * omega)) / (omega.shape[0] * np.max(A))
+				self.waveform[i] = np.sum(A * np.exp(i * self.decay) * np.sin(i * omega)) / (omega.shape[0] * A.max())
 
 	def getLabels(self) -> dict[str, list[Union[float, int]]]:
 		'''

test/src/test_samplers.py
@@ -162,8 +162,8 @@ class SamplerTests(TestCase):
 					# bounds on the Hamiltonian energy throughout the simulation.
 					model.generateWaveform()
 					self.assertFalse(np.isnan(model.waveform).any())
-					self.assertLessEqual(np.max(model.waveform), 1.)
-					self.assertGreaterEqual(np.min(model.waveform), -1.)
+					self.assertLessEqual(model.waveform.max(), 1.)
+					self.assertGreaterEqual(model.waveform.min(), -1.)
 
 	def test_poisson(self) -> None:
 		'''