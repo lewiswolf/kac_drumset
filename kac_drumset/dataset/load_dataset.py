e6b2b92fb192c0a921ddb7f44c3ba7253d559fb0
lewiswolf <lewiswolstanholme@gmail.com>
2023-04-28 21:38:21 +0100

    added try except for improved error handling

--- kac_drumset/dataset/load_dataset.py
+++ kac_drumset/dataset/load_dataset.py
@@ -27,48 +27,52 @@ def loadDataset(dataset_dir: str) -> TorchDataset:
 	loadDataset imports a kac_drumset dataset from the directory specified by the absolute path dataset_dir.
 	'''
 
-	with open(os.path.normpath(f'{dataset_dir}/metadata.json')) as file:
-		# import metadata
-		file.readlines(1)
-		dataset_size = int(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
-		representation_settings = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
-		sampler = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
-		sampler_settings = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
-		file.readlines(1)
-		# backwards compatibility
-		# delete during a major upgrade
-		if type(sampler) == str:
-			sampler = {
-				'name': sampler,
-				'version': '1.1.0',
-			}
-		# create dataset
-		dataset = TorchDataset(
-			dataset_dir=dataset_dir,
-			dataset_size=dataset_size,
-			representation_settings=representation_settings,
-			sampler=sampler,
-			sampler_settings=sampler_settings,
-			x_size=InputRepresentation.transformShape(
-				math.ceil(sampler_settings['duration'] * sampler_settings['sample_rate']),
-				representation_settings,
-			),
-		)
-		# import loop
-		printEmojis('Importing dataset... 📚')
-		with tqdm(total=dataset_size, bar_format=tqdm_settings['bar_format'], unit=tqdm_settings['unit']) as bar:
-			for i in range(dataset_size):
-				# import relevant information
-				file.readlines(1)
-				x = torch.as_tensor(json.loads(
-					file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1],
-				))
-				y = listToTensor(json.loads(
-					file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:],
-				))
-				file.readlines(1)
-				# append input features to dataset
-				dataset.__setitem__(i, x, y)
-				bar.update(1)
-		file.close()
+	try:
+		with open(os.path.normpath(f'{dataset_dir}/metadata.json')) as file:
+			# import metadata
+			file.readlines(1)
+			dataset_size = int(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+			representation_settings = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+			sampler = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+			sampler_settings = json.loads(file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1])
+			file.readlines(1)
+			# backwards compatibility
+			# delete during a major upgrade
+			if type(sampler) == str:
+				sampler = {
+					'name': sampler,
+					'version': '1.1.0',
+				}
+			# create dataset
+			dataset = TorchDataset(
+				dataset_dir=dataset_dir,
+				dataset_size=dataset_size,
+				representation_settings=representation_settings,
+				sampler=sampler,
+				sampler_settings=sampler_settings,
+				x_size=InputRepresentation.transformShape(
+					math.ceil(sampler_settings['duration'] * sampler_settings['sample_rate']),
+					representation_settings,
+				),
+			)
+			# import loop
+			printEmojis('Importing dataset... 📚')
+			with tqdm(total=dataset_size, bar_format=tqdm_settings['bar_format'], unit=tqdm_settings['unit']) as bar:
+				for i in range(dataset_size):
+					# import relevant information
+					file.readlines(1)
+					x = torch.as_tensor(json.loads(
+						file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:-1],
+					))
+					y = listToTensor(json.loads(
+						file.readlines(1)[0].replace('\n', '').split(':', 1)[1][1:],
+					))
+					file.readlines(1)
+					# append input features to dataset
+					dataset.__setitem__(i, x, y)
+					bar.update(1)
+			file.close()
+	except Exception as e:
+		if type(e).__name__ == 'IndexError':
+			raise IndexError('The dataset you are importing is corrupted.')
 	return dataset
