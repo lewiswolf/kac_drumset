7dbe47f02ba7e314ddaa6f90efd026940067ca7c
lewiswolf <lewiswolstanholme@gmail.com>
2024-08-10 17:25:09 +0100

    switched back to find_packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Custom build script used to import this package's metadata from both the readme 
 # core
 import codecs
 import os
-from setuptools import find_namespace_packages
+from setuptools import find_packages
 from skbuild import setup
 
 
@@ -65,7 +65,7 @@ setup(
 	keywords=['kac_drumset'],
 	long_description_content_type='text/markdown',
 	name=name,
-	packages=find_namespace_packages(),
+	packages=find_packages(),
 	package_dir={'kac_drumset': f'{os.path.relpath(os.path.dirname(__file__), os.getcwd())}/kac_drumset'},
 	package_data={'kac_drumset': ['py.typed']},
 	version=version,
